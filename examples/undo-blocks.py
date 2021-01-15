e6842ec51f26e2f53fdb7f0d25e68ce90fb67a06
Chris Guida <chrisguida@users.noreply.github.com>
2021-01-14 19:58:58 -0600

    Update undo-blocks.py

--- examples/undo-blocks.py
+++ examples/undo-blocks.py
@@ -10,6 +10,6 @@ for i, file_name in enumerate(undo_files):
     print("parsing undo file #%d" % i)
     for j, block_raw in enumerate(get_blocks(file_name)):
         undo_block_ctr += 1
-        if j % 1000 == 0 or (i == 1 and j > 9000):
+        if j % 1000 == 0:
             print("parsing undo block #%d in file #%d block #%d" % (undo_block_ctr, i, j))
         block_undo_current = BlockUndo(block_raw)
